43118ff4d5a6b6fb9657d4107fe527597f229d75
Hisaeri <roch.fiona@gmail.com>
2022-02-14 18:21:14 +0900

    Use MUILink for footer github link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,5 @@
-import { Box, Card, Container } from "@mui/material";
+import { Link as MUILink } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import Link from "next/link";
 
 type FooterProps = {};
@@ -25,7 +26,7 @@ const Footer = ({}: FooterProps) => {
           >
             {"Made with ❤️ by "}
             <Link passHref href="https://www.github.com/Hisaeri">
-              <a target="_blank">Hisaeri</a>
+              <MUILink target="_blank">Hisaeri</MUILink>
             </Link>
           </Box>
         </Container>
