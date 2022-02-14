379b83ebb74b094f5dc71e4fff0fb0ebd99fd219
Hisaeri <roch.fiona@gmail.com>
2022-02-14 18:17:42 +0900

    Use missingImg and PetImg inside PetComponent

--- components/pet.tsx
+++ components/pet.tsx
@@ -5,12 +5,11 @@ import {
   CardHeader,
   Link as MUILink,
 } from "@mui/material";
-import { HideImage } from "@mui/icons-material";
 import { Box } from "@mui/system";
-import Image from "next/image";
 import Link from "next/link";
 import { Pet } from "../types/pet";
-import { grey } from "@mui/material/colors";
+import PetImgComponent from "./petImg";
+import MissingImgComponent from "./missingImg";
 
 type PetComponentProps = {
   pet: Pet;
@@ -20,33 +19,12 @@ const PetComponent = ({ pet }: PetComponentProps) => {
   let petImage: JSX.Element = <></>;
   if (pet.primary_photo_cropped && pet.primary_photo_cropped.small) {
     petImage = (
-      <Image
-        objectFit={"cover"}
-        src={pet.primary_photo_cropped.small}
-        blurDataURL={pet.primary_photo_cropped.small}
-        layout="fill"
-        placeholder="blur"
-      />
+      <PetImgComponent
+        petImg={pet.primary_photo_cropped.small}
+      ></PetImgComponent>
     );
   } else {
-    petImage = (
-      <Box
-        sx={{
-          backgroundColor: grey[100],
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        <HideImage sx={{ color: "text.secondary" }} />{" "}
-        <Typography variant="body2" color="text.secondary">
-          No image
-        </Typography>
-      </Box>
-    );
+    petImage = <MissingImgComponent></MissingImgComponent>;
   }
 
   return (
