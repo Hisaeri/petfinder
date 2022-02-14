f3420ef35edc09a429ac1c9698027e0e8fe3416e
Hisaeri <roch.fiona@gmail.com>
2022-02-14 17:46:09 +0900

    Fix wrong font imports : import from app instead of index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,10 @@ import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/styles";
 import Layout from "../components/layout";
 import "../styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import Container from "@mui/material/Container";
 import Head from "next/head";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import { NextPage } from "next";
 import { Typography } from "@mui/material";
 
