7950628fb7a8beb8ab8d7a3513ecf6c05501edae
Fiona Roch <roch.fiona@gmail.com>
2022-03-29 09:30:11 +0200

    Switch pets list fetching to Client Side rendering

--- pages/pets.tsx
+++ pages/pets.tsx
@@ -1,55 +1,52 @@
 import { Typography, Box } from "@mui/material";
 import Container from "@mui/material/Container";
-import { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+import { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import PetListComponent from "../components/petList";
 import api from "../lib/api/api";
 import { PetfinderPets, PetfinderToken } from "../types/petfinder";
 
-export const getStaticProps: GetStaticProps = async () => {
-  let tokenResponse: PetfinderToken;
-  // Get Petfinder token
-  try {
-    const resToken = await api.postPetfinderToken();
-    tokenResponse = resToken.data;
-  } catch (e) {
-    return {
-      props: {
-        pets: null,
-        error: "Can't get Petfinder token: " + e,
-      },
-    };
-  }
-
-  try {
-    //   Get Petfinder pets data
-    const resData = await api.getPetfinderAnimals(tokenResponse.access_token);
-    const petsResponse: PetfinderPets = resData.data;
-    return {
-      props: {
-        pets: petsResponse.animals,
-      },
-    };
-  } catch (e) {
-    return {
-      props: {
-        revalidate: 60,
-        pets: null,
-        error: "Can't get list of pets:" + e,
-      },
-    };
-  }
-};
-
-const PetsPage: NextPage = ({
-  pets,
-  error,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+const PetsPage: NextPage = () => {
   let petsComponent: JSX.Element;
+
+  const [pets, setPets] = useState<PetfinderPets | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getPetFinderData = async () => {
+      let tokenResponse: PetfinderToken;
+      // Get Petfinder token
+      try {
+        const resToken = await api.postPetfinderToken();
+        tokenResponse = resToken.data;
+      } catch (e) {
+        setError("Can't get Petfinder token: " + e);
+        setLoading(false);
+        return;
+      }
+
+      try {
+        // Get Petfinder pets data
+        const resData = await api.getPetfinderAnimals(
+          tokenResponse.access_token
+        );
+        const petsResponse: PetfinderPets = resData.data;
+        setPets(petsResponse);
+        setLoading(false);
+      } catch (e) {
+        setError("Can't get list of pets:" + e);
+        setLoading(false);
+      }
+    };
+    getPetFinderData();
+  }, []);
+
   if (!pets) {
     petsComponent = <p>{error}</p>;
   } else {
-    petsComponent = <PetListComponent pets={pets}></PetListComponent>;
+    petsComponent = <PetListComponent pets={pets.animals}></PetListComponent>;
   }
 
   return (
@@ -61,6 +58,7 @@ const PetsPage: NextPage = ({
         <Typography variant="h2" component="h1">
           Pets who need a home
         </Typography>
+        {isLoading && <Typography component="body1">Loading...</Typography>}
         <Box sx={{ my: 4 }}>{petsComponent}</Box>
       </Container>
     </>
